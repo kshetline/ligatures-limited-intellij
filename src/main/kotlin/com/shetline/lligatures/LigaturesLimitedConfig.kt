3973e337aaec74c8a0745d478976c5dc507a613e
Kerry Shetline <kerry@shetline.com>
2020-06-23 21:07:00 -0400

    Partial work on settings dialog.

--- src/main/kotlin/com/shetline/lligatures/LigaturesLimitedConfig.kt
+++ src/main/kotlin/com/shetline/lligatures/LigaturesLimitedConfig.kt
@@ -1,20 +1,25 @@
 package com.shetline.lligatures
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
+import com.intellij.json.json5.Json5Language
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.options.Configurable
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
+import com.intellij.ui.LanguageTextField
 import javax.swing.JCheckBox
 import javax.swing.JComponent
 import javax.swing.JPanel
 import javax.swing.JTextField
 
-class LigaturesLimitedConfig : Configurable, Disposable {
+class LigaturesLimitedConfig(private val project: Project) : Configurable, Disposable {
   private lateinit var wrapper: JPanel
   private lateinit var panel: JPanel
   private lateinit var contexts: JTextField
   private lateinit var debug: JCheckBox
   private lateinit var disregarded: JTextField
+  private lateinit var json: LanguageTextField
+  private lateinit var advancedSettings: JCheckBox
 
   private val configState
     get() = LigaturesLimitedSettings.instance.state
@@ -35,6 +40,10 @@ class LigaturesLimitedConfig : Configurable, Disposable {
 
   override fun createComponent(): JComponent? {
     return wrapper
+  }
+
+  private fun createUIComponents() {
+    json = LanguageTextField(Json5Language.INSTANCE, project, "")
   }
 
   override fun reset() {
