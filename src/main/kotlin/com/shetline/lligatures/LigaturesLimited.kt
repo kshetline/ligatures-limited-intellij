1488329864759f6ccba9a018664ab6a9a6fa4190
Kerry Shetline <kerry@shetline.com>
2020-07-19 00:59:17 -0400

    Fix language matching.

--- src/main/kotlin/com/shetline/lligatures/LigaturesLimited.kt
+++ src/main/kotlin/com/shetline/lligatures/LigaturesLimited.kt
@@ -119,7 +119,7 @@ class LigaturesLimited : PersistentStateComponent<LigaturesLimited>, AppLifecycl
       index = (matchIndex + matchText.length).coerceAtLeast(index + 1)
       ProgressManager.checkCanceled()
 
-      if (elem == null || elem.language != file.language)
+      if (elem == null || elem.rootLanguage != file.rootLanguage)
         continue
 
       val category = ElementCategorizer.categoryFor(elem, matchText, matchIndex)
@@ -704,5 +704,14 @@ class LigaturesLimited : PersistentStateComponent<LigaturesLimited>, AppLifecycl
     }
 
     val Language.idLc get(): String = this.id.toLowerCase().replace(' ', '_')
+
+    val PsiElement.rootLanguage get(): Language {
+      var lang = this.language
+
+      while (lang.baseLanguage != null)
+        lang = lang.baseLanguage!!
+
+      return lang
+    }
   }
 }
