51d949205965a2b744feaaf5b7bbdaf9096f6c85
Kerry Shetline <kerry@shetline.com>
2020-07-28 15:58:18 -0400

    Work-around for more compatibility issue, this time with IU-202.6397.94.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.shetline"
-version = "1.0.1"
+version = "1.0.2"
 
 repositories {
   mavenCentral()
@@ -39,12 +39,10 @@ tasks {
   }
 }
 
-tasks.withType<Jar> {
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
-}
-
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
   changeNotes("""
+      <h2>1.0.1, 1.0.2</h2>
+      <ul><li>Updates for compatibility with various versions of IDEA</li></ul>
       <h2>1.0.0</h2>
       <ul><li>First stable release</li></ul>
 """
